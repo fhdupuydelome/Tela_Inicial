fc48c08aea6cd80a86473dcb992af7fcf26dc602
fernando.dupuy <fernando.dupuy@maxifrota.com.br>
2022-06-08 23:09:14 -0300

    rastro funcionando

--- app/src/main/java/com/example/tela_inicial/MapsActivity.java
+++ app/src/main/java/com/example/tela_inicial/MapsActivity.java
@@ -18,6 +18,11 @@ import android.view.View;
 import android.widget.Switch;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -29,6 +34,9 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.tela_inicial.databinding.ActivityMapsBinding;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionDeniedResponse;
@@ -36,17 +44,108 @@ import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    private static final int REQUEST_LAST_LOCATION = 1;
+    private static final int REQUEST_LOCATION_UPDATES = 2;
 
-    private GoogleMap mMap;
     private Geocoder geocoder;
+    private GoogleMap mMap;
     private ActivityMapsBinding binding;
-    private CardView cardAjustes;
-    GoogleMap googleMap;
-    private Marker currentLocationMaker;
+
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    private LocationRequest mLocationRequest;
+    private LocationCallback mLocationCallback;
+    private Location currentPosition, lastPosition;
+    private boolean firstFix = true;
+    private double distanciaAcumulada;
+
+    private Marker userMarker;
+    private Polyline rastro;
+
     private LatLng currentLocationLatLong;
 
+    private void centralizaMapa() {
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+            mFusedLocationProviderClient.getLastLocation().addOnSuccessListener(
+                    new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            atualizaPosicaoNoMapa(location);
+                        }
+                    }
+            );
 
+        } else {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LAST_LOCATION
+            );
+        }
+    }
+
+    private void iniciarColetaLocalizacao() {
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+            //Configurar o location request
+            mLocationRequest = LocationRequest.create();
+            mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+            mLocationRequest.setInterval(5 * 1000);
+            mLocationRequest.setFastestInterval(1 * 1000);
+            //registra a callback
+            mLocationCallback = new LocationCallback() {
+                @Override
+                public void onLocationResult(@NonNull LocationResult locationResult) {
+                    super.onLocationResult(locationResult);
+                    atualizaPosicaoNoMapa(locationResult.getLastLocation());
+
+                }
+            };
+
+            mFusedLocationProviderClient.requestLocationUpdates(mLocationRequest, mLocationCallback, null);
+
+        } else {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION_UPDATES
+            );
+        }
+    }
+
+    private void atualizaPosicaoNoMapa(@NonNull Location location) {
+        LatLng currentLatLng, lastLatLng;
+        if (firstFix) {
+            firstFix = false;
+            currentPosition = lastPosition = location;
+            lastLatLng = new LatLng(lastPosition.getLatitude(), lastPosition.getLongitude());
+            currentLatLng = new LatLng(currentPosition.getLatitude(), currentPosition.getLongitude());
+            distanciaAcumulada = 0;
+
+        } else {
+            lastLatLng = new LatLng(lastPosition.getLatitude(), lastPosition.getLongitude());
+            lastPosition = currentPosition;
+            currentPosition = location;
+            currentLatLng = new LatLng(currentPosition.getLatitude(), currentPosition.getLongitude());
+            distanciaAcumulada += currentPosition.distanceTo(lastPosition);
+        }
+        if (mMap != null) {
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 16.0f));
+            if (userMarker == null) {
+                userMarker = mMap.addMarker(new MarkerOptions().position(currentLatLng));
+            } else {
+                mMap.addPolyline(new PolylineOptions()
+                        .add(lastLatLng ,currentLatLng)
+                        .visible(true)
+                );
+                userMarker.setPosition(currentLatLng);
+                mMap.animateCamera(CameraUpdateFactory.newLatLng(currentLatLng));
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,89 +156,44 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+        assert mapFragment != null;
         mapFragment.getMapAsync(this);
 
-    }
+        iniciarColetaLocalizacao();
 
+    }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        //centralizaMapa();
+    }
 
-        LatLng salvador = new LatLng(-12.924164, -38.510258);
-        mMap.addMarker(new MarkerOptions()
-                .position(salvador)
-                .title("Bomfim"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(salvador));
-        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(salvador, 15));
-        mMap.setIndoorEnabled(true);
-        UiSettings mapUI = mMap.getUiSettings();
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-
-
-
-        googleMap.getUiSettings().setZoomControlsEnabled(true);
-        googleMap.getUiSettings().setCompassEnabled(true);
-        googleMap.getUiSettings().setZoomControlsEnabled(true);
-        googleMap.getUiSettings().setScrollGesturesEnabled(true);
-        googleMap.getUiSettings().setRotateGesturesEnabled(true);
-        googleMap.getUiSettings().setIndoorLevelPickerEnabled(true);
-
-
-
-
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
+        if (requestCode == REQUEST_LAST_LOCATION) {
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                centralizaMapa();
+            } else {
+                Toast.makeText(this, "Sem permissão para monitorar sua localização", Toast.LENGTH_SHORT)
+                        .show();
+                finish();
+            }
         }
-        googleMap.setMyLocationEnabled(true);
-
-    }
-
-    @Override
-    public void onLocationChanged(@NonNull Location location) {
-
-        if(currentLocationMaker != null){
-            currentLocationMaker.remove();
+        if (requestCode == REQUEST_LOCATION_UPDATES) {
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                iniciarColetaLocalizacao();
+            } else {
+                Toast.makeText(this, "Sem permissão para monitorar sua localização", Toast.LENGTH_SHORT)
+                        .show();
+                finish();
+            }
         }
-        currentLocationLatLong = new LatLng(location.getLatitude(),location.getLongitude());
-        MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(currentLocationLatLong);
-        markerOptions.title("localização Atual");
-        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
-        currentLocationMaker = mMap.addMarker(markerOptions);
-
-        CameraPosition cameraPosition = new CameraPosition.Builder().zoom(15).target(currentLocationLatLong).build();
-        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-
-        Toast.makeText(this, "Localização atualizada", Toast.LENGTH_SHORT).show();
-
-
-
     }
 
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
 
-    }
-
-    @Override
-    public void onProviderEnabled(@NonNull String provider) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(@NonNull String provider) {
-
-    }
 }
 
 
